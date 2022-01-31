84dc8ef5c18e305b01376430d1db41835520b773
codenamehemingway <jared.hemingway.jh@gmail.com>
2022-01-31 11:11:04 -0700

    adding pseudocode and standard boilerplate info

---
+++ assets/js/config.js
@@ -0,0 +1,4 @@
+var config = {
+  MY_KEY: "d3e5277fbf",
+  SECRET_KEY: "f376e9a968986ed05ba8fb",
+};
---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// global variables
+api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// search function tied to button taking from the input field
+
+// adding histroy via href nodes under search box
+
+// main weather display
+
+// 5 day forecast put into cards below main
