a6345eac471900dcdfc57611747671c9cc005be0
codenamehemingway <jared.hemingway.jh@gmail.com>
2022-02-01 12:50:35 -0700

    added variables to js calling id's and classes from html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
 // global variables
 var city = []
+var searchFormEl=document.querySelector("#search-form");
+var cityInputEl=document.querySelector("#city-input");
+var currentWeatherEl=document.querySelector("#weather-container");
+var citySearchOutputEl = document.querySelector("#searched-city");
+var forecastInfo = document.querySelector("#forecast");
+var multiDayForecast = document.querySelector("#multi-day-forecast")
+var multiDayContainerEl = document.querySelector("#multi-day-container");
+var priorSearchButtonEl = document.querySelector("#prior-search-buttons");
+var priorSearch= document.getElementsByClassName("prior-search");
+var searchBox = document.getElementsByClassName("search-box");
+var currentCity = document.getElementsByClassName("current-city");
+
 api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 // search function tied to button taking from the input field
 
